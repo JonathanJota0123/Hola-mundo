a1dfb3f89608cfc0fd6ae3007c872192d55ef40b
Jonathan Jota <jonathanalexanderjota9@gmail.com>
2025-08-20 17:31:19 -0500

    Beginning to learn javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+const titutlo = document.getElementById("titulo");
+console.log(titulo);
+const modificado = "hola " + titulo;
+console.log(modificado);
+
+const terapia = document.getElementById("terapia");
+console.log(terapia.textContent);
+const modificado2 = terapia.textContent + "chao";
+console.log(modificado2);
