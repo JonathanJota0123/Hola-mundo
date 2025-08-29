1e7dcaa34292eea2fe93e18668138e4f42fc1068
Jonathan Jota <jonathanalexanderjota9@gmail.com>
2025-08-29 13:32:37 -0500

    Starting to create js code

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-const titutlo = document.getElementById("titulo");
-console.log(titulo);
-const modificado = "hola " + titulo;
-console.log(modificado);
+const terapias = [
+  {
+    tipo: "Terapias personales",
+    descripcion: "Un viaje interno hacia la sanacion",
+    link: "/HTML/Services-personal.html",
+  },
+  {
+    tipo: "Terapias de pareja",
+    descripcion: "Reconstruir el amor juntos",
+    link: "/HTML/Services-parents.html",
+  },
+  {
+    tipo: "Terapia en familia",
+    descripcion: "La union como fuerza sanadora",
+    link: "/HTML/Services-parents.html",
+  },
+];
 
-const terapia = document.getElementById("terapia");
-console.log(terapia.textContent);
-const modificado2 = terapia.textContent + "chao";
-console.log(modificado2);
+const container = document.querySelector("#terapias-container");
+
+terapias.forEach((t) => {
+  const card = `
+    <div class="col-md-4">
+          <div class="card shadow-sm mb-4">
+            <div class="card-body">
+               <h5 class="card-title">${t.tipo}</h5>
+              <p class="card-text">${t.descripcion}</p>
+              <a ${t.link} class="btn btn-primary">Ver mas</a>
+            </div>
+          </div>`;
+  container.innerHTML += card;
+});
